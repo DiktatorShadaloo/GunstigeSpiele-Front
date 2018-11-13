3f49b73e98df5457f1e37af6cbba47d50e358ed5
Francisco <francisco.betancor.pallas@fing.edu.uy>
2018-11-13 18:23:20 -0300

    agrego el modulo reactstrap, y se agrega el header

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App-container">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <body>
-
-        </body> */}
-        <div className="App-container">
-          <div className="App-header">
-              <span className="App-header-logo-default" />
-          </div>
-          <div className="App-content">
-          </div>
+        <div className="Navbar">
+          <Navbar color="dark" dark>
+            <NavbarBrand href="/" className="Navbar-brand-text">
+              GunstigeSpiele
+            </NavbarBrand>
+            <form>
+              <div className="no-border input-group">
+                <input
+                  placeholder="Buscar..."
+                  type="text"
+                  className="form-control"
+                />
+              </div>
+            </form>
+          </Navbar>
         </div>
+        <div className="App-content" />
       </div>
     );
   }
